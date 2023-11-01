bbeccc14213e81b92233366e70bb24c35882ce63
Eric Schreiber <eric.schreiber@gmx.ch>
2023-11-01 18:00:07 +0100

    Adding models and implementing the OD.

--- task1/outlier_detectors.py
+++ task1/outlier_detectors.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class BaseOutlierDetector:
     def __init__(self):
         pass
@@ -7,3 +10,22 @@ class BaseOutlierDetector:
 
     def transform(self, X):
         return X
+    
+    def drop(self, X, y):
+        return X, y
+    
+class EricsVotingDetector:
+    def __init__(self, n_estimators=100, n_jobs=-1):
+        # Load pre-computed predictions in csv file task1/data/outliers_majority_vote_ocsvm_iforest_knn.csv
+        # and store them in self.outlier_predictions
+        self.outlier_predictions = pd.read_csv('data/outliers_majority_vote_ocsvm_iforest_knn.csv', index_col='id')
+
+    def fit(self, X, y):
+        return self
+
+    def drop(self, X, y):
+        # remove outliers from X and y (eg. the id's where self.outlier_predictions['outlier'] == 1.0)
+        X = X[self.outlier_predictions['outlier'] == 0.0]
+        y = y[self.outlier_predictions['outlier'] == 0.0]
+        return X, y
+    
