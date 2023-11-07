06abb361c7bb9cd5e603fc2c565d1153e5ecfc61
Eric Schreiber <eric.schreiber@inf.ethz.ch>
2023-11-07 14:41:01 +0100

    Grid Searched Parameters.

--- task1/configs/model_configs.py
+++ task1/configs/model_configs.py
@@ -11,6 +11,12 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RationalQuadratic, RBF, ConstantKernel as C
 from sklearn.ensemble import StackingRegressor
 from sklearn.linear_model import RidgeCV
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import BayesianRidge
+
+import numpy as np
 
 RANDOM_SEED = 3141592
 
@@ -24,6 +30,51 @@ model_configs = {
             "subsample": 0.45,
         },
         "param_grid": {},
+    },
+    "gbr_big_grid": {
+        # GradientBoostingRegressor model.
+        "model": GradientBoostingRegressor,
+        "model_hyperparams": {
+            "criterion": "squared_error", 
+            "learning_rate": 0.05, 
+            "max_depth": 8, 
+            "max_features": 1.0,
+            "min_samples_leaf": 1, 
+            "min_samples_split": 0.1, 
+            "n_estimators": 500, 
+            "subsample": 0.7
+        },
+        "param_grid": {
+            # "model__n_estimators": [100, 500, 1000, 2000],#grid3 from here
+            # "model__learning_rate": [0.1,0.2],#[0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2],
+            # "model__subsample": [0.8,0.9,1.0],#[0.45, 0.5, 0.55, 0.7, 0.9, 1.0],
+            # # "model__min_samples_split": np.linspace(0.1, 3, 12),#grid2 from here
+            # # "model__min_samples_leaf": np.linspace(0.1, 2, 12),
+            # # "model__max_depth":[3,5,8],
+            # # "model__max_features":[0.5,0.8,0.9,0.95,1.0],# Grid1 from here
+            # # "model__criterion": ["friedman_mse",  "squared_error"],
+            # # "model__subsample":[0.4, 0.45, 0.5, 0.55, 0.6, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
+
+            # # Second try here with the best parameters from the first try
+            # # "model__criterion": ["squared_error"],
+            # # "model__max_features":[1.0],
+            # # "model__max_depth":[6,8],
+            # # "model__min_samples_split": [0.1,1,2],
+            # # "model__min_samples_leaf": [0.1,0.5,1],
+            # # "model__learning_rate": [0.1,0.05],
+            # # "model__subsample": [0.7,0.8,0.9,1],
+            # # "model__n_estimators": [500],
+            # # Last gridsearch try 5 here
+            # "model__criterion": ["squared_error"],
+            # "model__max_features":[1.0],
+            # "model__max_depth":[6,8],
+            # "model__min_samples_split": [0.1,1,2],
+            # "model__min_samples_leaf": [0.1,0.5,1],
+            # "model__learning_rate": [0.1,0.05],
+            # "model__subsample": [0.7,0.8,0.9,1.0],
+            # "model__n_estimators": [500,600,700,1000],
+
+        },
     },
     "catboost": {
         # Catboost model.
@@ -55,17 +106,27 @@ model_configs = {
     "adaboost": {
         # Adaboost model.
         "model": AdaBoostRegressor,
-        "model_hyperparams": {"n_estimators": 500, "learning_rate": 0.5},
-        "param_grid": {},
+        "model_hyperparams": {
+            "n_estimators": 550,
+            "learning_rate": 1.1,
+            "estimator": DecisionTreeRegressor(),
+        },
+        "param_grid": {
+        #  "model__n_estimators": [500,550,600],
+        #  "model__learning_rate":[0.8,0.9,1,1.1,1.2],
+        #  "model__estimator": [DecisionTreeRegressor(), SVR(kernel=RationalQuadratic()), SGDRegressor(), BayesianRidge()],
+        },
     },
-    "lgbmregressor_grid": {
+    "lgbmregressor": {
         # LGBMRegressor model.
         "model": LGBMRegressor,
-        "model_hyperparams": {},
+        "model_hyperparams": {
+            "min_gain_to_split": 0.01,
+        },
         "param_grid": {
-            "model__max_depth": [5, 6, 7, 8, 9],
-            "model__num_leaves": [32, 64, 70, 90, 100, 128, 140], # between 2^(max_depth-1) and 2^max_depth
-            "model__min_data_in_leaf": [10, 100, 500, 750, 1000, 1500, 2000], # to avoid overfitting
+            # "model__max_depth": [5, 6, 7, 8, 9],
+            # "model__num_leaves": [32, 64, 70, 90, 100, 128, 140], # between 2^(max_depth-1) and 2^max_depth
+            # "model__min_data_in_leaf": [10, 100, 500, 750, 1000, 1500, 2000], # to avoid overfitting
         },
     },
     "svr": {
@@ -77,8 +138,17 @@ model_configs = {
     "gpregressor": {
         # GaussianProcessRegressor model.
         "model": GaussianProcessRegressor,
-        "model_hyperparams": {"kernel": RationalQuadratic()},
-        "param_grid": {},
+        "model_hyperparams": {
+                "kernel": RationalQuadratic(alpha=1, length_scale=1),
+                "random_state": RANDOM_SEED,
+                "n_restarts_optimizer": 5,
+                "normalize_y": True,
+        },
+        "param_grid": {
+            # # "model__n_restarts_optimizer": [1, 2, 3, 4, 5, 6, 8, 10, 14],
+            # "model__normalize_y": [False, True],
+            # "model__kernel": [RationalQuadratic(), RBF(), C(1.0, (1e-3, 1e3)) * RationalQuadratic()],
+        },
     },
     "stackingregressor": {
         # StackingRegressor model.
@@ -109,9 +179,9 @@ model_configs = {
                 ("svr", SVR(kernel=RationalQuadratic())),
                 ("catboost", CatBoostRegressor(depth=6, learning_rate=0.1, iterations=100)),
                 ("xgboost", XGBRegressor(learning_rate=0.1, max_depth=6, min_child_weight=1)),
-                ("adaboost", AdaBoostRegressor()),
-                ("lgbmregressor", LGBMRegressor(max_depth=6, num_leaves=45, min_data_in_leaf=10)),
-                ("gpregressor", GaussianProcessRegressor(kernel=RationalQuadratic())),
+                ("adaboost", AdaBoostRegressor(n_estimators=550, learning_rate=1.1, estimator=DecisionTreeRegressor())),
+                ("lgbmregressor", LGBMRegressor(min_gain_to_split=0.01, max_depth=6, num_leaves=45, min_data_in_leaf=10)),
+                ("gpregressor", GaussianProcessRegressor(kernel=RationalQuadratic(alpha=1, length_scale=1), random_state=RANDOM_SEED, n_restarts_optimizer=5, normalize_y=True)),
             ],
         },
         "param_grid": {},
